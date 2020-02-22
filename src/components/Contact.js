623048ff9bec7f7ed34e3fcf2e183746d4debebb
Marcielo Bell <marcielobell@gmail.com>
2020-02-22 12:23:10 -0800

    Contact Page added

---
+++ src/components/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../style/Contact.css'
+
+
+const Contact = () => {
+    return (
+        <div className="contact-page">
+       <header className="contact-header">
+           Contact Us
+       </header>
+        <section className="contact-form-container">
+         {/* Add Contact Form Details Here  */}
+        </section>
+        </div>
+    )
+}
+
+
+export default Contact
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import '../style/Footer.css'
 const Footer = () => {
     return (
         <div className='footer-page'>
-            <p className="copy-text">© 2020 Bellmarc  Design & Development. All rights reserved.
+            <p className="copy-text">© 2020 Bellmarc Design & Development. All rights reserved.
             </p>
         </div>
     )
