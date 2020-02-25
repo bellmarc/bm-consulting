347cada8c92470e84850791af09b7b260e423ca0
Marcielo Bell <marcielobell@gmail.com>
2020-02-24 21:49:27 -0800

    Services added

---
+++ src/components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../style/Services.css';
+
+
+const Services = () => {
+    return (
+        <div className="services-page">
+        <p>Services offered Sketch file</p>
+        </div>
+    )
+}
+
+
+export default Services
