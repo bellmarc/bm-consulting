2d7bc4d2ee599df7bc513f7a82f3a4b346e65d83
Marcielo Bell <marcielobell@gmail.com>
2020-02-25 23:53:39 -0800

    work in progress for splash screen HOC

---
+++ src/components/withSplash.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import'../style/withSplash.css';
+
+function LoadingScreen(){
+    return (
+        <div className="splash-screen">
+            One moment while your app loads.
+            <div className="loading-ani">~</div>
+        </div>
+    )
+}
+
+function withSplash() {
+    return class extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                loading: true
+            }
+        }
+
+        // async componentDidMount(){
+        // try {
+
+        // }}
+
+    }
+}
+
+
